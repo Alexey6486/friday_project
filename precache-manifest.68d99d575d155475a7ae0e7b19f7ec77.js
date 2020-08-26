b3c86ac59abbf7278eb7f807deb280e9be133b5b
Alexey <alexey6486@gmail.com>
2020-08-26 09:25:15 +0300

    Updates

---
+++ precache-manifest.68d99d575d155475a7ae0e7b19f7ec77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e05e790fe68cbad4a40040f917ec2fb",
+    "url": "/friday_project/index.html"
+  },
+  {
+    "revision": "48261d8c51678735c929",
+    "url": "/friday_project/static/css/main.1db0541a.chunk.css"
+  },
+  {
+    "revision": "cd5f13349aa3d439e31f",
+    "url": "/friday_project/static/js/2.b4056624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/friday_project/static/js/2.b4056624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48261d8c51678735c929",
+    "url": "/friday_project/static/js/main.5f4e0939.chunk.js"
+  },
+  {
+    "revision": "a97e04ee65846022ecc5",
+    "url": "/friday_project/static/js/runtime-main.4be45700.js"
+  }
+]);
