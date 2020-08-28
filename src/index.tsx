32ec4d29c0182e1773e7f5f2ac427abf2acea492
Alexey <alexey6486@gmail.com>
2020-08-28 14:54:06 +0300

    first_friday/all_tasks_done/Dev_branch_for_local_development/master_for_gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './app/App';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import { store } from './store/store';
 
 ReactDOM.render(
-    <HashRouter basename="/">
+    <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
     ,
     document.getElementById('root')
 );
