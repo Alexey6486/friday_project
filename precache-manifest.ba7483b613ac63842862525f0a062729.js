1f029177a80e685b435babc22f7d1992af97904f
Alexey <alexey6486@gmail.com>
2020-08-28 14:17:56 +0300

    Updates

--- precache-manifest.ba7483b613ac63842862525f0a062729.js
+++ precache-manifest.ba7483b613ac63842862525f0a062729.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "557ae58cbd7fae0ceeeffbf96cd8ca39",
+    "revision": "b29dac6764e59cb8b73ab5942dbf052b",
     "url": "/friday_project/index.html"
   },
   {
-    "revision": "f8821432dbfd1a72a6ba",
+    "revision": "60a6f89b61be2a3b173b",
     "url": "/friday_project/static/css/main.c6c6a7c0.chunk.css"
   },
   {
-    "revision": "324bcf7b9bb811f4e625",
-    "url": "/friday_project/static/js/2.73f94aea.chunk.js"
+    "revision": "5e76b2432a4442328f41",
+    "url": "/friday_project/static/js/2.bdd007dc.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/friday_project/static/js/2.73f94aea.chunk.js.LICENSE.txt"
+    "url": "/friday_project/static/js/2.bdd007dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f8821432dbfd1a72a6ba",
-    "url": "/friday_project/static/js/main.db36e679.chunk.js"
+    "revision": "60a6f89b61be2a3b173b",
+    "url": "/friday_project/static/js/main.66fc0ae3.chunk.js"
   },
   {
     "revision": "a97e04ee65846022ecc5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/friday_project/precache-manifest.134ae0490f3e03e38e27e9317928c2cc.js"
+  "/friday_project/precache-manifest.ba7483b613ac63842862525f0a062729.js"
 );
 
 self.addEventListener('message', (event) => {
